96efee95c83fef477b5bb04ab9666b74a943527f
Gerijacki <gerardloriz05@gmail.com>
2025-11-22 19:40:47 +0100

    feat: Add contact route

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\GameController;
 use App\Http\Controllers\GeminiController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [GameController::class, 'index'])->name('home');
 Route::post('/analyze', [GameController::class, 'analyze'])->name('analyze');
+Route::get('/contact', [ContactController::class, 'index'])->name('contact');
 
 Route::get('/gemini/test', [GeminiController::class, 'test']);
